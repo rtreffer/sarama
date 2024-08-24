2bad38e9b93426393fa9e5e03af2e09ede68298d
René Treffer <treffer@measite.de>
2024-08-24 16:06:40 +0200

    Demonstrate zstd encoders > GOMAXPROCS

--- zstd_test.go
+++ zstd_test.go
@@ -1,7 +1,10 @@
 package sarama
 
 import (
+	"fmt"
 	"runtime"
+	"sync"
+	"sync/atomic"
 	"testing"
 )
 
@@ -26,4 +29,80 @@ func BenchmarkZstdMemoryConsumption(b *testing.B) {
 		// zstdEncMap.Delete(params)
 	}
 	runtime.GOMAXPROCS(gomaxprocsBackup)
+}
+
+// BenchmarkZstdEncoderCreation benchmarks the creation of a zstd encoders
+// under high concurrency and low GOMAXPROCS. In an ideal scenario we would
+// never have more than 1 encoder per GOMAXPROCS.
+func TestZstdEncoderCreation(t *testing.T) {
+	gomaxprocsBackup := runtime.GOMAXPROCS(1)
+	defer runtime.GOMAXPROCS(gomaxprocsBackup)
+
+	goroutines := 500
+	blockSize := 1024
+	var startBarrier sync.WaitGroup
+	startBarrier.Add(goroutines)
+
+	type result struct {
+		goroutineID     int
+		compressedBytes int
+		encoders        int
+	}
+
+	encoderValues := make(chan result, goroutines)
+
+	mutex := &sync.Mutex{}
+	var encoders atomic.Int32
+
+	for i := 0; i < goroutines; i++ {
+		id := i
+		go func(id int) {
+			// generate a 1kb buffer and fill it with random data
+			buf := make([]byte, blockSize)
+			for i := 0; i < len(buf); i++ {
+				buf[i] = byte(i + id)
+			}
+
+			startBarrier.Done()
+			startBarrier.Wait()
+
+			mutex.Lock()
+			encoder := getZstdEncoder(ZstdEncoderParams{Level: 3})
+			currentEncoders := encoders.Add(1)
+			mutex.Unlock()
+
+			output := encoder.EncodeAll(buf, nil)
+			len := len(output)
+
+			mutex.Lock()
+			releaseEncoder(ZstdEncoderParams{Level: 9}, encoder)
+			encoders.Add(-1)
+			mutex.Unlock()
+
+			encoderValues <- result{id, len, int(currentEncoders)}
+
+		}(id)
+	}
+
+	maxEncoderValues := 0
+	maxEncoersGoroutineId := 0
+	totalCompressedBytes := 0
+	totalBytes := 0
+	for i := 0; i < goroutines; i++ {
+		result := <-encoderValues
+		totalCompressedBytes += result.compressedBytes
+		totalBytes += blockSize
+		if result.encoders > maxEncoderValues {
+			maxEncoderValues = result.encoders
+			maxEncoersGoroutineId = result.goroutineID
+		}
+	}
+
+	fmt.Printf("Max encoders: %d, goroutine ID: %d\n", maxEncoderValues, maxEncoersGoroutineId)
+	fmt.Printf("Total compressed bytes: %d, total bytes: %d\n", totalCompressedBytes, totalBytes)
+	fmt.Printf("Compression ratio: %f\n", float64(totalCompressedBytes)/float64(totalBytes))
+
+	if maxEncoderValues > 1 {
+		t.Fatalf("Expected at most 1 encoder per GOMAXPROCS, got %d", maxEncoderValues)
+	}
 }
